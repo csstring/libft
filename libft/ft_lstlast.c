b4bb8696b829afc9f992d1b0114651d4cf0b737a
schoe <97031320+csstring@users.noreply.github.com>
2022-03-10 21:58:58 +0900

    Add files via upload

---
+++ libft/ft_lstlast.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: schoe <schoe@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/10 11:04:07 by schoe             #+#    #+#             */
+/*   Updated: 2022/03/10 11:04:09 by schoe            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*temp;
+
+	temp = lst;
+	if (temp == NULL)
+		return (temp);
+	while (temp -> next)
+		temp = temp -> next;
+	return (temp);
+}
